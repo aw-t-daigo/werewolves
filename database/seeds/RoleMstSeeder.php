a341c052ccae7b802b110962d619605b0a734dd7
醍醐 崇紘 <t_daigo@active.co.jp>
2018-08-08 15:32:43 +0900

    update: fix DDL and seeder:

--- database/seeds/RoleMstSeeder.php
+++ database/seeds/RoleMstSeeder.php
@@ -12,13 +12,13 @@ class RoleMstSeeder extends Seeder
     public function run()
     {
         $roleMst = [
-            ['role_name' => '村人', 'is_wolf' => false],
-            ['role_name' => '人狼', 'is_wolf' => true],
-            ['role_name' => '占い師', 'is_wolf' => false],
-            ['role_name' => '霊能者', 'is_wolf' => false],
-            ['role_name' => '共有', 'is_wolf' => false],
-            ['role_name' => '狩人', 'is_wolf' => false],
-            ['role_name' => '狂人', 'is_wolf' => false],
+            ['role_name' => '村人', 'is_wolf' => false, 'need_manip' => false],
+            ['role_name' => '人狼', 'is_wolf' => true, 'need_manip' => true],
+            ['role_name' => '占い師', 'is_wolf' => false,'need_manip' => true],
+            ['role_name' => '霊能者', 'is_wolf' => false, 'need_manip' => false],
+            ['role_name' => '共有', 'is_wolf' => false, 'need_manip' => false],
+            ['role_name' => '狩人','is_wolf' =>false, 'need_manip' => true],
+            ['role_name' => '狂人', 'is_wolf' => false, 'need_manip' => false],
         ];
 
         DB::table('role_mst')->insert($roleMst);
